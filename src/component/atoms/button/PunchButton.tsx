323e3c2c4b2789d409315f390f8633087fd2047d
azhimattsu <90542867+azhimattsu@users.noreply.github.com>
2022-01-26 00:14:58 +0900

    打刻ボタンの作成（途中）

--- src/component/atoms/button/PunchButton.tsx
+++ src/component/atoms/button/PunchButton.tsx
@@ -1,6 +1,6 @@
 import { memo, ReactNode, VFC } from "react";
 import styled from "styled-components";
-
+import punch from "../../../icons/logo.png";
 type Props = {
   children: ReactNode;
   src: string;
@@ -9,13 +9,16 @@ type Props = {
 
 const SButton = styled.div`
   position: relative;
-  display: inline-block;
   font-weight: bold;
+  font-size: 4em;
   padding: 0.25em 0.5em;
   text-decoration: none;
   color: #00bcd4;
   background: #ececec;
   transition: 0.4s;
+  width: 200px;
+  height: 200px;
+
   :hover {
     background: #00bcd4;
     color: white;
@@ -23,11 +26,18 @@ const SButton = styled.div`
   }
 `;
 
+const SImg = styled.img`
+  width: 64px;
+`;
 export const PunchButton: VFC<Props> = memo((props) => {
   const { children, onClick } = props;
   return (
     <>
-      <SButton onClick={onClick}>{children}</SButton>
+      <SButton onClick={onClick}>
+        <SImg src={punch} alt="" />
+        <br />
+        {children}
+      </SButton>
     </>
   );
 });
