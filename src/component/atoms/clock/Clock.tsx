a578fa8b766df2ab9b6eeb7350a7603aa2898c46
azhimattsu <90542867+azhimattsu@users.noreply.github.com>
2022-01-30 00:01:07 +0900

    打刻画面の作成

---
+++ src/component/atoms/clock/Clock.tsx
@@ -0,0 +1,40 @@
+import { memo, useEffect, useState, VFC } from "react";
+import styled from "styled-components";
+import { interval } from "rxjs";
+
+const SContainer = styled.div`
+  background-color: white;
+  height: 100%;
+`;
+
+const SDay = styled.div`
+  font-size: 32px;
+  text-align: center;
+`;
+const STime = styled.div`
+  font-size: 64px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const Clock: VFC = memo(() => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const subscription = interval(1000).subscribe(() => {
+      setDate(new Date());
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  return (
+    <SContainer>
+      <SDay>{`${date.getFullYear()} / ${
+        date.getMonth() + 1
+      } / ${date.getDate()}`}</SDay>
+      <STime>{`${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`}</STime>
+    </SContainer>
+  );
+});
--- src/component/pages/WebPunch.tsx
+++ src/component/pages/WebPunch.tsx
@@ -1,20 +1,73 @@
-import { ChangeEvent, memo, useState, VFC } from "react";
+import { memo, VFC } from "react";
 import styled from "styled-components";
 import { PunchButton } from "../atoms/button/PunchButton";
+import { Clock } from "../atoms/clock/Clock";
 import ImgIn from "../../icons/logo.png";
+
+const SContainer = styled.div`
+  display: grid;
+  grid-template-rows: 200px 200px 200px 200px;
+  grid-template-columns: 100px 200px 200px 200px 200px 100px;
+`;
+
+const SItem1 = styled.div`
+  background-color: red;
+  grid-row: 1;
+  grid-column: 1 / span 7;
+`;
+
+const SItem2 = styled.div`
+  background-color: green;
+  grid-row: 2;
+  grid-column: 2;
+`;
+
+const SItem3 = styled.div`
+  background-color: blue;
+  grid-row: 2;
+  grid-column: 3;
+`;
+
+const SItem4 = styled.div`
+  background-color: pink;
+  grid-row: 3;
+  grid-column: 2 / span 2;
+`;
+
+const SItem5 = styled.div`
+  background-color: white;
+  grid-row: 2 / span 2;
+  grid-column: 4 / span 2;
+`;
 
 export const WebPunch: VFC = memo(() => {
   const onClickIn = () => {
-    alert("TEST");
+    alert("出勤");
+  };
+  const onClickOut = () => {
+    alert("退勤");
   };
   return (
     <>
-      <PunchButton src={ImgIn} onClick={onClickIn}>
-        出勤
-      </PunchButton>
-      <PunchButton src={ImgIn} onClick={onClickIn}>
-        退勤
-      </PunchButton>
+      <SContainer>
+        <SItem1>
+          <Clock />
+        </SItem1>
+        <SItem2>
+          <PunchButton src={ImgIn} onClick={onClickIn}>
+            出勤
+          </PunchButton>
+        </SItem2>
+        <SItem3>
+          <PunchButton src={ImgIn} onClick={onClickOut}>
+            退勤
+          </PunchButton>
+        </SItem3>
+        <SItem4>
+          <textarea cols={50} rows={10} maxLength={20} />
+        </SItem4>
+        <SItem5>地図</SItem5>
+      </SContainer>
     </>
   );
 });
