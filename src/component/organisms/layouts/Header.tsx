8444fcd90229ad475f8a4f5c88236ef77c9a5264
azhimattsu <90542867+azhimattsu@users.noreply.github.com>
2022-02-11 14:36:01 +0900

    ヘッダーメニューの作成（配置）

--- src/component/organisms/layouts/Header.tsx
+++ src/component/organisms/layouts/Header.tsx
@@ -1,5 +1,6 @@
 import { memo, VFC } from "react";
 import styled from "styled-components";
+import { useLoginUser } from "../../../hooks/useLoginUser";
 import logoimg from "../../../icons/logo.png";
 
 const SHeader = styled.header`
@@ -14,10 +15,41 @@ const SLogoImg = styled.img`
   height: 32px;
 `;
 
+const SNav = styled.nav`
+  position: fixed;
+  text-align: right;
+  top: 0;
+  right: 0px;
+  width: 100%;
+`;
+const SUl = styled.ul`
+  display: inline-block;
+  padding: 10px;
+`;
+const Sli = styled.ul`
+  display: inline-block;
+  font-size: 16px;
+  padding: 10px;
+`;
 export const Header: VFC = memo(() => {
+  const { loginUser } = useLoginUser();
+
   return (
     <>
-      <SHeader>ジオる!勤怠</SHeader>
+      <SHeader>
+        ジオる!勤怠 ログインユーザ：
+        {`${loginUser?.LastName} ${loginUser?.FirstName}`}
+        <SNav>
+          <SUl>
+            <Sli>
+              <a href="">出勤簿</a>
+            </Sli>
+            <Sli>
+              <a href="">ログアウト</a>
+            </Sli>
+          </SUl>
+        </SNav>
+      </SHeader>
     </>
   );
 });
