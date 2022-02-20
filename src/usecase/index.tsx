55047257c5a6f5c82bfb0bac1d6709cf5368b787
azhimattsu <90542867+azhimattsu@users.noreply.github.com>
2022-02-20 21:13:46 +0900

    不要ファイルの削除

--- src/usecase/index.tsx
+++
@@ -1,8 +0,0 @@
-import { UserGetInteractor } from "./users/userGetUsecase";
-import { UserDriverImpl } from "../domain/driver/userdriver";
-import { UserGateway } from "../domain/repository/userrepository";
-
-import { useUserGetUseCase, UserGetAtom } from "../hooks/useUserGetUsecase";
-import { useRecoilState } from "recoil";
-
-const [userUseCase, setUserUseCase] = useRecoilState(UserGetAtom);
